8f1aa4cf00462ca33e1bd452f52b050fe8e81708
egruman <ethan2065@gmail.com>
2015-10-06 14:23:10 -0400

    how should the remaining move positions be modified if there is a collision?

--- turnManager.cs
+++ turnManager.cs
@@ -1,5 +1,5 @@
-getReady(): readyPlayers+=1;
-getReadyPlayers(): return readyPlayers;
+void getReady(){ readyPlayers+=1;}
+list<Player> getReadyPlayers(){ return readyPlayers;}
 void Update(){
 	if (readyPlayers == PhotonNetwork.playerList.Length && !action) {
 		SendNetworkMessage("Turn starts!");
@@ -8,14 +8,14 @@ void Update(){
 	}
 }
 IEnumerator startTurn(){
-	yield return new WaitForSeconds (stepTime);
-	time+=1
+	time+=1;
+	yield return new WaitForSeconds(stepTime);
 }
-endTurn(){
-	time=-1
+void endTurn(){
+	time=-1;
 }
-getTime(): return time;
-getTurn(): return turn;
+int getTime(){ return time;}
+int getTurn(){ return turn;}
 
 public list<Vector2>[] calculateTurnSequence(list<Vector2> movement1,list<Vector2> movement2)
 {
@@ -33,14 +33,13 @@ public list<Vector2>[] calculateTurnSequence(list<Vector2> movement1,list<Vector
 		if(boardManager.isOccupied(movement2[i])){
 			velocitySequences[1][i]=0-vel2;}
 		else{velocitySequences[1][i]=vel2;}
-		if(movement1==movement2)
-		{
+		if(movement1[i]==movement2[i]){
 			Vector2 temp=velocitySequences[0][i];
 			velocitySequences[0][i]=velocitySequences[1][i];
 			velocitySequences[1][i]=temp;
-		}
-		
+		}	
 	}
-
+	movement1[i]=movement1[i-1]+vel1;
+	movement2[i]=movement2[i-1]+vel2;  //how should the remaining movement positions be updated?
 	return velocitySequences;
 }
