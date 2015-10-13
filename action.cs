a6c95bb46561712812906f83031211f8039ab6f2
matthew9671 <yixiuz@andrew.cmu.edu>
2015-10-12 22:57:29 -0400

    Update action.cs
    Added weaponId, extraMovement

--- action.cs
+++ action.cs
@@ -1,5 +1,10 @@
-public class action : MonoBehaviour {
-	Vector2 moveTarget;
-	Vector2 actTarget;
+using UnityEngine;
 
+public class action {
+	public Vector2 movement;
+	public Vector2 attack;
+	public int weaponId;
+	public Vector2 extraMovement;	// You get extraMovement when you fire a weapon with recoil
+	// Movement points to where you are at the end of turn
+	// Use movement-extraMovement to find out where you were when you fired the weapon
 }
