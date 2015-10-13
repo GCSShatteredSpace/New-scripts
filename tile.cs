b506ea4328cb47ddf41f440cae6819ec516562ac
matthew9671 <yixiuz@andrew.cmu.edu>
2015-10-12 23:00:06 -0400

    Update tile.cs
    Changed some of the names

--- tile.cs
+++ tile.cs
@@ -2,22 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class tile : MonoBehaviour {
-	private Renderer rend;
+	Renderer rend;
 
-	private Vector2 playerPosition;
+	Vector2 playerPosition;
 	public Vector2 tilePosition;
 
-	private bool chosen;
-	private bool current;
-	private bool rightHold;
+	bool chosen;
+	bool current;
+	bool inTarget;
+	bool rightHold;
+	[SerializeField]bool valid;
 
 	[SerializeField] Color mouseOverColor;
 	[SerializeField] Color chosenColor;
 	[SerializeField] Color attackColor;	
-	[SerializeField] Color invaildColor;
+	[SerializeField] Color invalidColor;
 	[SerializeField] Color jumpColor;
-
-	[SerializeField] bool shootable;
+	
 	[SerializeField] bool mouseOn;
 
 	[SerializeField] inputManager iManager;
@@ -25,30 +26,36 @@ public class tile : MonoBehaviour {
 	[SerializeField] functionManager SS;
 	
 	int weapon;
-	playerAction player;
+	player myPlayer;
 
 	void Start() {
-		rend = GetComponent<Renderer>();
+		// This part is necessary for any spawned prefab
+		// This will change to "gameController(Clone)" if we decide to instantiate the gameController
+		GameObject gameController = GameObject.Find ("gameController");
+		iManager = gameController.GetComponent<inputManager> ();
+		//tManager = gameController.GetComponent<turnManager> ();
+		SS = gameController.GetComponent<functionManager> ();
+		dataBase = gameController.GetComponent<statsManager> ();
 
-		//establish connection with other scripts
+		rend = GetComponent<Renderer>();
 	}
 
 	void OnMouseEnter() {
-		if (!iManager.isCommandable)
+		if (!iManager.isCommandable())
 						return;
-		if (iManager.hold && shootable) {     // Target selection mode
+		if (iManager.isInTargetMode() && iManager.hasAttackLeft()) {     // Target selection mode
 	 		iManager.attackCommand(tilePosition);// Everytime the mouse enters another tile the attack position is refreshed
 		}
 		mouseOn = true;
 	}
 	
 	void OnMouseExit() {  // Clears the state on previous tile
-		mouseon = false;
+		mouseOn = false;
 		rightHold = false;		
 	}
 
 	void OnMouseOver(){
-		if (!iManager.commandable)
+		if (!iManager.isCommandable())
 			return;
 		if (Input.GetButton ("Fire2") && (current)) {   // Detect right MouseUp event manually
 			rightHold = true;
@@ -61,39 +68,38 @@ public class tile : MonoBehaviour {
 	}
 
 	void OnMouseDown() {
-		if (!iManager.commandable)
+		if (!iManager.isCommandable())
 			return;
-
 		if (current) {
-			iManager.startHold(tilePosition);					// Enters targeting mode
+			iManager.startTargeting(tilePosition);					// Enters targeting mode
 		}
 	}
 
   // The MouseUp event only works on left mouse button
 	void OnMouseUp(){     // Mouseup implies a Mousedown action, which means the player clicked on the tile
-		iManager.stopHold();
+		iManager.endTargeting();
 		if (valid && mouseOn) {   // MouseUp always trigger on the tile where MouseDown happens!
 			iManager.moveCommand(tilePosition);   // Left mouse click choses a new movement step
 		}
 	}
 
 	void Update(){ 								//this part is what determines the display on each tile
-    if (!iManager.commandable)
+    	if (!iManager.isCommandable())
 			return;
-		playerPosition = iManager.playerPosition;	
-		//in the case of weapons with recoil effect, fireposition!=playerposition
+		playerPosition = iManager.getPlayerPosition();	
+			//in the case of weapons with recoil effect, fireposition!=playerposition
 
-    setState();
-    setAppearance();
-    
+	    setState();
+	    setAppearance();
 	}
 	
 	void setState(){  // Sets the states of the current tile
-	  if (SS.near(playerPosition, tilePosition) && iManager.hasMoveLeft()) {
+	  	if (SS.isNear(playerPosition, tilePosition) && iManager.hasMoveLeft()) {
 			valid = true;
 		} else {
 			valid = false;
 		}
+		// Check if the tile is in movement path
 		if (tilePosition == playerPosition){  
 			current = true;
 			chosen = true;
@@ -105,36 +111,52 @@ public class tile : MonoBehaviour {
 				chosen = false;
 			}
 		}
+
+		if (iManager.inTarget (tilePosition)) {
+			inTarget = true;
+		} else {
+			inTarget=false;
+		}
 	}
 	
 	void setAppearance(){
-	  if (iManager.inTarget(tilePosition)) {
+		clear ();
+	  	if (inTarget) {
 			setTarget();
 		}
 		if (current) {
 			setCurrent();
-		}else if(chosen||(mouseon&&valid)){
+		}else if(chosen||(mouseOn&&valid)){
 			setMouseOver();
-		}else{
-			clear();
 		}
 		// In target selection mode
-		if (gctrl.hold) {
-		  wpnId = iManager.getWeapon();
-		  weapon wpn=player.getWeapon(wpnId);
-		  if (wpn.isInRange(SS.getDistance(tilePosition))){
-		    setInvalid();
-		  }
+		if (iManager.isInTargetMode()) {
+		  int wpnId = iManager.getWeaponId();
+		  //weapon wpn = myPlayer.getWeapon(wpnId);
+		  //if (wpn.isInRange(SS.getDistance(tilePosition,playerPosition))){
+		  //  setInvalid();
+		  //}
 		}
 	}
+
+	// We can do something fancy here in the future
 	void setTarget(){
+		rend.material.color = attackColor;
 	}
+
 	void setCurrent(){
+		rend.material.color = chosenColor;
 	}
+
 	void setMouseOver(){
+		rend.material.color = mouseOverColor;
 	}
-	void setInvaild(){
+
+	void setInvalid(){
+		rend.material.color = invalidColor;
 	}
+
 	void clear(){
+		rend.material.color = Color.white;
 	}
 }
