508ea4e5ead0aa14a5d58971ddf2f04013c72cd2
matthew9671 <yixiuz@andrew.cmu.edu>
2015-10-12 23:11:25 -0400

    Create statsManager.cs

---
+++ statsManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class statsManager : MonoBehaviour {
+
+	// These are all important game-related parameters
+	public float tileSize;
+	public float stepTime;
+	public int turretRange;
+	public int playerStartEnergy;
+
+	public int mapSize;
+
+}
