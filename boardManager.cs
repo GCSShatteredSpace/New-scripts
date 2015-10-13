6cec0d1fc20ce7dedbd8aefbbd9365564ce86f78
matthew9671 <yixiuz@andrew.cmu.edu>
2015-10-12 23:05:55 -0400

    Update boardManager.cs
    Added the method for generating a hexmap.

--- boardManager.cs
+++ boardManager.cs
@@ -10,17 +10,37 @@ using UnityEngine.UI;
 public class boardManager : MonoBehaviour {
 
     public Vector2[] direction;
-    const float vertPart = 0.8666f; // sqrt(3)/2
-    public int tileSize;
-    [SerializeField] float piecesize //I assume this is the diameter of the circle as well?
+    const float ratio = 0.8666f; // sqrt(3)/2
+	float tileSize; //I assume this is the diameter of the circle as well?
     [SerializeField] statsManager dataBase;
     [SerializeField] functionManager SS;    // Just call it SS for sake of laziness
     [SerializeField] Vector2[] turretSpawnPoint = new Vector2[5];
     [SerializeField] Vector2[] barrierSpawnPoint = new Vector2[12]; //barrier numbers may vary
 
+	[SerializeField] GameObject gameTile;
+
     void Start(){
         tileSize = dataBase.tileSize;
+		generateHexMap (dataBase.mapSize);
     }
+
+	// This will be changed since we won't be satisfied with just a hexagon in the future
+	// But the formula for locating tiles is useful!
+	void generateHexMap (int size){
+		Transform boardHolder = new GameObject ("Board").transform;
+		Vector3 pieceposition;
+		for(int i=1;i<size*2;i++){
+			for(int j=1;j<2*size-Mathf.Abs(size-i);j++){
+				//crazy math formulas for the actuall positions, don't delve in if you value your life!
+				pieceposition= new Vector3 (tileSize*(Mathf.Abs(size-i)*0.5f+j-size),tileSize*ratio*(i-size),0f);
+				GameObject instance = Instantiate(gameTile,pieceposition,Quaternion.identity) as GameObject;
+				tile tileInfo = instance.GetComponent<tile>();
+				//the same craziness here
+				tileInfo.tilePosition=new Vector2(j-0.5f*(size+i-Mathf.Abs(i-size)),i-size);
+				instance.transform.SetParent (boardHolder);
+			}
+		}
+	}
 
     /*
      * Copied from http://answers.unity3d.com/questions/62644/distance-between-a-ray-and-a-point.html
@@ -45,30 +65,30 @@ public class boardManager : MonoBehaviour {
     /*
      * Returns whether the projectile is blocked by barriers
      */
-    public bool isBlocked(Vector2 firePosition, Vection2 targetPosition){
+    public bool isBlocked(Vector2 firePosition, Vector2 targetPosition){
         int count=0;
         Vector3 firePos = SS.hexPositionTransform(firePosition);
         Vector3 targetPos = SS.hexPositionTransform(targetPosition);
         Vector3 barrierPos;
         float dis;
-        for (int i=0;i<barrierSpawnPoint.Length;i++){
-            barrierPos = SS.hexPositionTransform(barrierSpawnPoint[i]);
-            if ((firePos.x<=barrierPos.x) && 
-                (targetPos.x>=barrierPos.x) &&
-                (firePos.y<=barrierPos.y) && 
-                (targetPos.y>=barrierPos.y)){ //make sure the barrier is inside the parallelogram
-                dis = DistancePointLine(barrierPos, firePos,targetPos);
-                if (dis<piecesize/2){
-                    return true; //the projectile cut through one 
-                }
-                else if (dis == piecesize/2){   // Nice try! There's actually a (Mathematically speaking) simple solution:
-                    count++;                    // if dis==piecesize/2 (or almostEqual) nudge the target a little bit away from the current barrier
-                }                               // And run this thing again!
-            }
-        if (count>1){ //the projectile cuts through the tangent of multiple barriers
-                      //actually this idea is bugged in some barrier formations
-            return true;
-        }
+        for (int i=0; i<barrierSpawnPoint.Length; i++) {
+			barrierPos = SS.hexPositionTransform (barrierSpawnPoint [i]);
+			if ((firePos.x <= barrierPos.x) && 
+				(targetPos.x >= barrierPos.x) &&
+				(firePos.y <= barrierPos.y) && 
+				(targetPos.y >= barrierPos.y)) { //make sure the barrier is inside the parallelogram
+				dis = DistancePointLine (barrierPos, firePos, targetPos);
+				if (dis < tileSize / 2) {
+					return true; //the projectile cut through one 
+				} else if (dis == tileSize / 2) {   // Nice try! There's actually a (Mathematically speaking) simple solution:
+					count++;                    // if dis==piecesize/2 (or almostEqual) nudge the target a little bit away from the current barrier
+				}                               // And run this thing again!
+			}
+			if (count > 1) { //the projectile cuts through the tangent of multiple barriers
+				//actually this idea is bugged in some barrier formations
+				return true;
+			}
+		}
         return false;
     }
 
